c82f6472a58f6c84a9b53cea2bda78be452f7599
Johan Fernando Aponte Valencia <wdjohanwd@gmail.com>
2024-05-15 09:39:09 +0200

    Creadas Persona, Muggle, Mago

--- Hogwarts/src/Mago.java
+++ Hogwarts/src/Mago.java
@@ -1,13 +1,19 @@
 public class Mago extends Persona {
     private String tipoEscoba;
-    
+
     public Mago(String nombre, String fechaNac) {
         super(nombre, fechaNac);
-
     }
 
     @Override
     public Boolean esMayor() {
-        return edad >=17;
+        return edad >= 17;
+    }
+
+    @Override
+    public String toString() {
+        return "El nombre del mago es: " + super.getNombre() + ", la fecha de" + " nacimiento es:" +
+                " " + super.getFechaNac() + "y el tipo de " + "escoba es: " + this.tipoEscoba;
+
     }
 }
--- Hogwarts/src/Persona.java
+++ Hogwarts/src/Persona.java
@@ -2,6 +2,8 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public abstract class Persona {
+
+
     private String nombre;
     private LocalDate fechaNac;
     protected int edad;
@@ -24,5 +26,13 @@ public abstract class Persona {
     public String toString() {
 
         return "El nombre de la persona es: " + this.nombre + ", y la fecha de nacimiento es: " + this.fechaNac;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
     }
 }
