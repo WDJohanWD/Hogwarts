3a3801524a7cfa14415477d5a2819fcb73ea0077
Johan Aponte <wdjohanwd@gmail.com>
2024-05-20 23:35:17 +0200

    Clase DAO empezada

--- Hogwarts/src/DAO.java
+++ Hogwarts/src/DAO.java
@@ -1,3 +1,26 @@
-public class DAO    {
+import java.sql.*;
+
+public class DAO {
+    private String url;
+    private String username;
+    private String password;
+
+    public DAO(String url, String user, String pass) {
+
+        this.url = url;
+        this.username = user;
+        this.password = pass;
+
+    }
+
+
+    public void crearAlumno(Mago mago) {
+        String sql = "INSERT "
+        try (Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/hogwarts", this.username, this.password)) {
+
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+
+    }
 }
- 
